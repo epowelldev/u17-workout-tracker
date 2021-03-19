f6db529bad6129a679a96cd22fd13549fdc33a91
Erickson Powell <ericksonpowell@gmail.com>
2021-03-19 15:52:14 -0700

    changed list to plural (exercises over exercise)

--- models/Workout.js
+++ models/Workout.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: { type: Date, default: Date.now, },
-  exercise:[//array of exercises(documents)
+  exercises :[//array of exercises(documents)
     {
       type: { type: String, required: [true, "Type of exercise is required."]},
       name: { type: String, required: [true, "Name of exercise is required."]},
