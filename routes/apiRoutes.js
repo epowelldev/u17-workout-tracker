1c1fb2534afe1db73f065c1d715c6661972c5c6b
Erickson Powell <ericksonpowell@gmail.com>
2021-03-19 17:08:25 -0700

    added apiRoute for api/workouts/range

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,12 +5,8 @@ const {Workout} = require("../models");
 router.get("/api/workouts", (req, res) => {
   Workout
     .find()
-    .then(workouts => {
-      res.json(workouts);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+    .then(workouts => { res.json(workouts); })
+    .catch((err) => { res.status(500).json(err); });
   }
 );
 
@@ -23,32 +19,24 @@ router.put("/api/workouts/:id", (req, res) => {
       {$push: { exercises: newExercise }},
       {new: true}
     )
-    .then((workout) => {
-      res.json(workout);
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+    .then((workout) => { res.json(workout); })
+    .catch((err) => { res.status(500).json(err); });
   }
 );
 
 router.post("/api/workouts", (req, res) => {
   Workout
     .create(req.body)
-    .then((workout) => {
-      res.json(workout);
-    })
-    .catch((err) => {
-      res.status().json(err);
-    });
+    .then((workout) => { res.json(workout); })
+    .catch((err) => { res.status(500).json(err); });
   }
 );
 
 router.get("/api/workouts/range", (req, res) => {
   Workout
-  .find()
-  .then()
-  .catch();
+  .find().limit(7)
+  .then(workouts => { res.json(workouts); })
+  .catch((err) => { res.status(500).json(err); });
 });
 
 module.exports = router;
